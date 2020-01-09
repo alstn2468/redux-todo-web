d2912b9537711f5dde674277fe3ebe420c0de1b5
alstn2468 <alstn2468_@naver.com>
2020-01-09 14:43:52 +0900

    #5 Change method name handleOnclick -> onClickCompletedStatusButton

--- todo/src/Components/Todo.js
+++ todo/src/Components/Todo.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 import actionCreators from "../redux/action";
 import { connect } from "react-redux";
 
 function Todo({ item, dispatch }) {
-    function handleOnclick(event) {
+    function onClickCompletedStatusButton(event) {
         dispatch(actionCreators.changeTodoItemCompleted(item));
     }
+
+    useEffect(() => {
+        console.log(`${item.id} 화면에 출력`);
+
+        return () => {
+            console.log(`${item.id} 화면에서 삭제`);
+        };
+    }, [item]);
+
+    console.log(item);
+    console.log(typeof item);
 
     return (
         <div>
             {item.isCompleted.toString()} / {item.id} / {item.text}
-            <button onClick={event => handleOnclick(event)}>
+            <button onClick={event => onClickCompletedStatusButton(event)}>
                 {item.isCompleted ? "COMPLETED" : "UNCOMPLETED"}
             </button>
         </div>
@@ -21,11 +32,11 @@ function Todo({ item, dispatch }) {
 Todo.propTypes = {
     item: PropTypes.objectOf(
         PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            text: PropTypes.string.isRequired,
-            isCompleted: PropTypes.bool.isRequired
+            id: PropTypes.string,
+            text: PropTypes.string,
+            isCompleted: PropTypes.bool
         })
-    ).isRequired
+    )
 };
 
 export default connect()(Todo);
