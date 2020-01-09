9c5cfd96d15e6a7d969d98e595984e6a22033cf7
alstn2468 <alstn2468_@naver.com>
2020-01-09 13:55:30 +0900

    #2 Create GlobalStyles.js for reset css

---
+++ todo/src/Components/GlobalStyles.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const globalStyles = createGlobalStyle`
+    ${reset};
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+    * {
+        box-sizing: border-box;
+    }
+    body {
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        color: rgba(20, 20, 20, 1);
+        background-color: white;
+    }
+`;
+
+export default globalStyles;
