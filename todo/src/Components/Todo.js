efb8978a65b6bd2d289d9779ee9814973fee273e
alstn2468 <alstn2468_@naver.com>
2020-01-09 17:06:48 +0900

    #18 Remove useless import useEffect

--- todo/src/Components/Todo.js
+++ todo/src/Components/Todo.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import DeleteButton from "./DeleteButton";
