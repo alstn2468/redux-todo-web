360c1cf6e6e49587a5e26c17ea6e526fd554ddbb
Kimminsu <alstn2468_@naver.com>
2020-09-14 13:29:55 +0900

    Remove ClearCompletedButton container using hooks

--- todo/src/Components/ClearCompletedButton/index.js
+++ todo/src/Components/ClearCompletedButton/index.js
@@ -1,3 +1,38 @@
-import ClearCompletedButtonContainer from './ClearCompletedButtonContainer';
+import React, { useCallback } from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchClearCompletedTodoItem } from "actions/todoAction";
 
-export default ClearCompletedButtonContainer;
+const Button = styled.button`
+    display: flex;
+    flex-direction: flex-start;
+    font-size: 18px;
+    margin-left: 10px;
+
+    @media (min-width: 320px) and (max-width: 480px) {
+        font-size: 10px;
+        margin-left: 5px;
+        font-weight: 500;
+    }
+`;
+
+function ClearCompletedButton() {
+    const dispatch = useDispatch();
+    const { isLoggedIn } = useSelector((state) => state.authReducer);
+    const onClickClearCompletedButton = useCallback(
+        () => dispatch(fetchClearCompletedTodoItem()),
+        []
+    );
+
+    return (
+        <Button
+            disabled={!isLoggedIn}
+            className={`header-button${isLoggedIn ? " able" : ""}`}
+            onClick={onClickClearCompletedButton}
+        >
+            CLEAR COMPLETED
+        </Button>
+    );
+}
+
+export default ClearCompletedButton;
