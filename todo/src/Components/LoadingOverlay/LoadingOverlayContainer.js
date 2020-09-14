3826cb7da8c50085803f4d6c27b8d1fcac252a50
Kimminsu <alstn2468_@naver.com>
2020-09-14 13:31:41 +0900

    Remove LoadingOverlay container

--- todo/src/Components/LoadingOverlay/LoadingOverlayContainer.js
+++
@@ -1,8 +0,0 @@
-import { connect } from "react-redux";
-import LoadingOverlayComponent from "Components/LoadingOverlay/LoadingOverlayComponent";
-
-function mapStateToProps(state) {
-    return state.todoReducer;
-}
-
-export default connect(mapStateToProps)(LoadingOverlayComponent);
--- todo/src/Components/LoadingOverlay/index.js
+++ todo/src/Components/LoadingOverlay/index.js
@@ -1,3 +1,28 @@
-import LoadingOverlayContainer from "Components/LoadingOverlay/LoadingOverlayContainer";
+import React from "react";
+import { useSelector } from "react-redux";
+import BaseLoadingOverlay from "react-loading-overlay";
 
-export default LoadingOverlayContainer;
+function LoadingOverlay() {
+    const { isFetching } = useSelector((state) => state.todoReducer);
+
+    return (
+        <BaseLoadingOverlay
+            active={isFetching}
+            spinner
+            fadeSpeed={500}
+            styles={{
+                spinner: (base) => ({
+                    ...base,
+                    width: 80,
+                }),
+                content: (base) => ({
+                    ...base,
+                    fontSize: 33,
+                }),
+            }}
+            text="Loading..."
+        />
+    );
+}
+
+export default LoadingOverlay;
